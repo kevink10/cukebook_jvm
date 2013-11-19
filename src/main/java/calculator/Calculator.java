a3652f868e166919e00bbeb8352f497d92fba9b1
Kevin Knowles <knowles.kevin@gmail.com>
2013-11-18 18:25:16 -0600

    first scenario running all steps but fails then step

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,11 +3,9 @@ package calculator;
 public class Calculator {
 
 	public Calculator(String input) {
-		// TODO Auto-generated constructor stub
 	}
 
 	public String run() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
--- src/test/java/calculator/CalculatorStepDefs.java
+++ src/test/java/calculator/CalculatorStepDefs.java
@@ -1,27 +1,29 @@
 package calculator;
 
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import cucumber.runtime.PendingException;
 
 public class CalculatorStepDefs {
 	private String input;
-	private String result;
+	private String calculatedResult;
 	
 	@Given("^the input \"([^\"]*)\"$")
-	public void the_input(String arg1) throws Throwable {
-	    input = arg1;
+	public void the_input(String input) throws Throwable {
+	    this.input = input;
 	}
 
 	@When("^the calculator is run$")
 	public void the_calculator_is_run() throws Throwable {
 		Calculator calc = new Calculator(input);
-		String result = calc.run();
+		calculatedResult = calc.run();
 	}
 
 	@Then("^the output is \"([^\"]*)\"$")
-	public void the_output_is(String arg1) throws Throwable {
-	    throw new PendingException();
+	public void the_output_is(String expectedResult) throws Throwable {
+	    assertThat(expectedResult, is(calculatedResult));
 	}
 }
