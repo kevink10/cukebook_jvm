8fb63a09afe2084b0ea96b6f83043628ac895dab
Kevin Knowles <knowles.kevin@gmail.com>
2013-11-18 19:05:48 -0600

    Minimal refactoring

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,6 +2,8 @@ package calculator;
 
 public class Calculator {
 	private String input;
+	private static final String ADDITION_SIGN_REGEX = "\\+";
+	private static final String DIVISION_SIGN_REGEX = "/";
 
 	public Calculator(String input) {
 		this.input = input;
@@ -9,16 +11,20 @@ public class Calculator {
 
 	public String run() {
 		int result = 0;
-		if (input.contains("+")) {
-			String[] addens = input.split("\\+");
+		if (isAddition()) {
+			String[] addens = input.split(ADDITION_SIGN_REGEX);
 			result = Integer.parseInt(addens[0])
 					+ Integer.parseInt(addens[1]);
 		} else {
-			String[] numbers = input.split("/");
+			String[] numbers = input.split(DIVISION_SIGN_REGEX);
 			result = Integer.parseInt(numbers[0])
 					/ Integer.parseInt(numbers[1]);
 		}
 		return String.valueOf(result);
+	}
+
+	private boolean isAddition() {
+		return input.contains("+");
 	}
 
 }
